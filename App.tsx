322362857defca8f8576482e7763d5aaa21da3cc
Pedro Santos <pedrohrsantos01@gmail.com>
2021-09-25 15:12:21 -0300

    Criação da Interface de SignIn

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-import {View, Text, TextInput} from "react-native";
+import React from 'react';
+import { SignIn } from './src/screens/SignIn';
 
-import { styles } from "./src/screens/SignIn/styles";
 
-export default function App()  {
+export default function App() {
   return(
-    <View style = {styles.container}>
-        <Text>
-            Vem com o papai!
-        </Text>
-    </View>
-  )}
+    <SignIn />
+  )
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import {View, 
+        Text,
+        Image,
+        StatusBar,
+ } from "react-native";
 
-import {View, Text, TextInput, } from "react-native";
+import { ButtonIcon } from "../../components/Buttonicon";
+import { styles } from "./styles";
+import IllustrationImg from "../../assets/illustration.png"
 
-export default function SignIn()  {
-    <View>
-        <Text>
-            Vem com o papai!
-        </Text>
+export function SignIn()  { 
+    return(
+    <View style = {styles.container}> 
+        <StatusBar
+        barStyle = 'light-content'
+        backgroundColor = 'transparent'
+        translucent
+
+        />
+        <Image 
+        source = {IllustrationImg} 
+        style = {styles.image} 
+        resizeMode = 'stretch'
+        />
+
+
+        <View style = {styles.content}>
+            <Text style = {styles.title}>
+            Organize {`\n`}
+            suas jogatinas {`\n`}
+            facilmente
+            </Text>
+
+            <Text style = {styles.subtitle}>
+            Crie grupos para jogar seus games{`\n`}
+            favoritos com seus amigos
+            </Text>
+
+
+            <ButtonIcon
+             title = 'Entrar com Discord'
+             activeOpacity={0.8}
+             />
+
+        </View>
     </View>
-}
+    )}
