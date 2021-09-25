aefdd33cbf7db48b35cb4f93f4e26920bcdbebda
Pedro Santos <pedrohrsantos01@gmail.com>
2021-09-25 02:05:21 -0300

    primeiros componentes

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, TextInput} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Agora vai funcionar!
-      </Text>
-      <StatusBar style="auto" />
+import { styles } from "./src/screens/SignIn/styles";
+
+export default function App()  {
+  return(
+    <View style = {styles.container}>
+        <Text>
+            Vem com o papai!
+        </Text>
     </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  )}
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import {View, Text, TextInput, } from "react-native";
+
+export default function SignIn()  {
+    <View>
+        <Text>
+            Vem com o papai!
+        </Text>
+    </View>
+}
